8e3ca182b7c8919674b74ad5206fe028e700cc4b
Ilya Meignan--Masson <ilya.meignan-masson@mailoo.org>
2026-03-31 14:48:38 +0000

    adding flake for demo

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "DuckDB development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "duckdb-dev";
+
+          packages = with pkgs; [
+            cmake
+            ninja
+            pkg-config
+            gcc
+            clang
+            python3
+            ccache
+            clang-tools
+            git
+          ];
+        };
+      });
+}
